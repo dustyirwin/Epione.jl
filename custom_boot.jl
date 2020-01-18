44ec7f6541ff5e58bc34d9208302461c55acf828
dusty <dusty.irwin@gmail.com>
2020-01-18 15:27:25 -0800

    added custom_boot.jl

---
+++ custom_boot.jl
@@ -0,0 +1,28 @@
+using Pkg; Pkg.activate(".")
+using Genie
+using SearchLight
+using PyCall
+using Dates: now, Second
+using Plots
+
+#include("genie.jl") BROKEN??
+
+include("./app/resources/sleeps/Sleeps.jl")
+include("./app/resources/sleeps/SleepsController.jl")
+include("./app/resources/depressions/Depressions.jl")
+include("./app/resources/depressions/DepressionsController.jl")
+include("./app/resources/patients/Patients.jl")
+include("./app/resources/patients/PatientsController.jl")
+include("./config/secrets.jl")
+include("./src/messages.jl")
+include("./src/routes.jl")
+include("./src/texting.jl")
+
+haskey(ENV, "HOST") || (ENV["HOST"] = "0.0.0.0")
+haskey(ENV, "GENIE_ENV") || (ENV["GENIE_ENV"] = "dev")
+
+SearchLight.Configuration.load_db_connection() |> SearchLight.Database.connect!
+
+Genie.startup()
+
+################################################################################
--- scratch.jl
+++ scratch.jl
@@ -1,31 +1,5 @@
-using Pkg; Pkg.activate(".")
-using Genie
-using SearchLight
-using PyCall
-using Dates: now, Second
-using Plots
-
-#include("genie.jl") BROKEN??
-
-include("./app/resources/sleeps/Sleeps.jl")
-include("./app/resources/sleeps/SleepsController.jl")
-include("./app/resources/depressions/Depressions.jl")
-include("./app/resources/depressions/DepressionsController.jl")
-include("./app/resources/patients/Patients.jl")
-include("./app/resources/patients/PatientsController.jl")
-include("./config/secrets.jl")
-include("./src/messages.jl")
-include("./src/routes.jl")
-include("./src/texting.jl")
-
-haskey(ENV, "HOST") || (ENV["HOST"] = "0.0.0.0")
-haskey(ENV, "GENIE_ENV") || (ENV["GENIE_ENV"] = "dev")
-
-SearchLight.Configuration.load_db_connection() |> SearchLight.Database.connect!
-
-startup()
-
-################################################################################
+@time include("./custom_boot.jl")
+#@time include("./genie.jl")
 
 SearchLight.Migration.status()
 
@@ -36,7 +10,12 @@ Sleeps.Sleep(MRN=3, date=string(timestamp), hours=8.5) |> save!
 Depressions.Depression(MRN=3, date=string(timestamp), score=5.5) |> save!
 
 all(Patients.Patient)
-all(Sleeps.Sleep)
+
+
+pat_2 = SearchLight.findone(Patients.Patient, MRN=2)
+sleeps_for_2 = [s for s in all(Sleeps.Sleep) if s.MRN==2]
+
+
 all(Depressions.Depression)
 
 dep_data = DepressionsController.get_depression_data_by_MRN(2)
@@ -46,7 +25,7 @@ q = PatientsController.get_patient_plot_by_MRN(3)
 savefig(q, "./public/plots/1.png")
 
 pats = 20
-time_to_process_manually_in_mins = 5
+time_to_process_manually_in_mins = 3.5
 labor_cost_per_min = 35/60
 outreach_days_per_month = 30.5
 cost_savings = pats *
